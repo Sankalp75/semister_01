23101d6969329265273e1e67ac0a4aafb97b4dde
Sanchan75 <sankalp75shambharkar@gmail.com>
2025-11-30 21:24:58 +0530

    Update assassin_14.c

--- assassin_14.c
+++ assassin_14.c
@@ -8,20 +8,18 @@ int main() {
     printf("Enter a website link: ");
     fgets(website, sizeof(website), stdin);
 
-    // Remove newline if present
     website[strcspn(website, "\n")] = '\0';
 
     length = strlen(website);
 
-    // Check if last 4 characters are ".com"
-    if (length >= 4 &&
+       if (length >= 4 &&
         website[length - 4] == '.' &&
         website[length - 3] == 'c' &&
         website[length - 2] == 'o' &&
         website[length - 1] == 'm') {
         printf("Valid link: ends with .com\n");
     } else {
-        printf("Invalid link: does not end with .com\n");
+                 printf("Invalid link: does not end with .com\n");
     }
 
     return 0;
