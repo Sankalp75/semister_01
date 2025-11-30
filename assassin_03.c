76abf2e487c506b675f723d19ff7a26965b79fb6
Sanchan75 <sankalp75shambharkar@gmail.com>
2025-11-30 21:20:28 +0530

    Update assassin_03.c

--- assassin_03.c
+++ assassin_03.c
@@ -6,13 +6,11 @@ int main() {
 
     printf("Enter first string: ");
     fgets(str1, sizeof(str1), stdin);
-    str1[strcspn(str1, "\n")] = '\0';  // remove newline
+    str1[strcspn(str1, "\n")] = '\0';  
 
     printf("Enter second string: ");
     fgets(str2, sizeof(str2), stdin);
-    str2[strcspn(str2, "\n")] = '\0';  // remove newline
-
-    // Concatenate str2 onto str1
+    str2[strcspn(str2, "\n")] = '\0'; 
     strcat(str1, str2);
 
     printf("Connected string: %s\n", str1);
