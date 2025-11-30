1932b24fa7eb63af0722b1d68cc5549805c02d6b
Sanchan75 <sankalp75shambharkar@gmail.com>
2025-11-30 21:23:54 +0530

    Update and rename assassin_12 to assassin_12.c

--- assassin_12.c
+++ assassin_12.c
@@ -5,24 +5,22 @@
 int main() {
     char sentence[500];
     int i, words = 0;
-    int inWord = 0;  // flag to track if we are inside a word
+    int inWord = 0;  
 
     printf("Enter a sentence: ");
     fgets(sentence, sizeof(sentence), stdin);
 
-    // Traverse the sentence
     for (i = 0; sentence[i] != '\0'; i++) {
         if (!isspace(sentence[i]) && sentence[i] != '\n') {
             if (inWord == 0) {
-                words++;       // new word starts
-                inWord = 1;    // now inside a word
+                words++;      
+                inWord = 1;   
             }
         } else {
-            inWord = 0;        // space or newline ends the word
+            inWord = 0;      
         }
     }
-
-    printf("Total number of words: %d\n", words);
+  printf("Total number of words: %d\n", words);
 
     return 0;
 }
