f15f4f3ad4f90488dd26adcbb2505082d9688561
Sanchan75 <sankalp75shambharkar@gmail.com>
2025-11-30 21:24:32 +0530

    Update assassin_13.c

--- assassin_13.c
+++ assassin_13.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <ctype.h>   // for isdigit()
+#include <ctype.h>  
 
 int main() {
     char mobile[100];
@@ -9,10 +9,8 @@ int main() {
     printf("Enter your mobile number: ");
     fgets(mobile, sizeof(mobile), stdin);
 
-    // Initialize pointer to start of string
     ptr = mobile;
 
-    // Traverse string using pointer
     while (*ptr != '\0') {
         if (isdigit(*ptr)) {
             count++;
@@ -20,9 +18,8 @@ int main() {
         ptr++;
     }
 
-    printf("Total digits in mobile number: %d\n", count);
+              printf("Total digits in mobile number: %d\n", count);
 
-    // Simple validity check (example: Indian mobile numbers usually have 10 digits)
     if (count == 10) {
         printf("Valid mobile number (10 digits).\n");
     } else {
